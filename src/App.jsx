f18c41a27c5920e1003ba78087d08e3deb5aca12
s.shahraeini@yahoo.com <s.shahraeini@yahoo.com>
2023-10-24 12:12:58 +0330

    add files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+return(
+<div className="container">
+      <h1>Hello</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button>Submit</button>
+    </div>
+
+)
+  
+}
+
+export default App

045023a57277fa0c598fc2820c728e0016914cee
s.shahraeini@yahoo.com <s.shahraeini@yahoo.com>
2023-10-24 12:25:11 +0330

    change h1 text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
 return(
 <div className="container">
-      <h1>Hello</h1>
+      <h1>oooo</h1>
       <input type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>
